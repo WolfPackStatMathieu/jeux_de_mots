561f4421c6fc62a3df37c762fdc65d229b286a49
GUERINEAU Apolline <id1980@DOMENSAI.ECOLE>
2022-10-14 10:57:37 +0200

    partie

---
+++ business_objects/partie.py
@@ -0,0 +1,8 @@
+class Partie :
+    def __init__(self, id_partie, mot_objectif, liste_mots_proposes, est_liste_perso, id_liste, score):
+        self.id_partie=id_partie
+        self.mot_objectif=mot_objectif
+        self.liste_mots_proposes=liste_mots_proposes
+        self.est_liste_perso=est_liste_perso
+        self.id_liste=id_liste
+        self.score=score
