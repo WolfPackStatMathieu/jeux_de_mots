bb7db99f4365be92a667346d2c3e5e08e7c761e1
DINH Linh-da <id1966@DOMENSAI.ECOLE>
2022-10-14 12:45:13 +0200

    travail sur la classe JoueurDAO

---
+++ dao/joueur_dao.py
@@ -0,0 +1,14 @@
+from utils.singleton import Singleton
+from dao.db_connection import DBConnection
+
+class JoueurDAO():
+    ''
+    def creer(joueur):
+        connection = DBConnection().connection
+        with connection.cursor() as cursor :
+            curseur.execute(
+                "INSERT INTO joueur(id_joueur, nom_joueur)"
+                " VALUES (id_joueur, nom_joueur)",
+                { "id_joueur" : joueur.id_joueur,
+                "nom_joueur" : joueur.nom_joueur}
+            )
