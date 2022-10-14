2bf22c617ef9ffaa9d6ee1a88a36a84a022b7b80
MARHFOUL Oussama <id2176@DOMENSAI.ECOLE>
2022-10-14 12:29:02 +0200

    petite modification du code de la classe joueur

--- Class_Joueur.py
+++ Class_Joueur.py
@@ -1,9 +1,11 @@
 class Joueur :
-    def __init__(self, id_joueur : int, nom_joueur : str, topten : list(float)) -> None:
+    def __init__(self, id_joueur : int, nom_joueur : str, topten : list()) -> None:
         self.id = id_joueur
         self.nom = nom_joueur
         self.topten = topten
     
     def __str__(self) -> str:
-        return f"l'identifiant du joueur est {self.id} \n le nom du joueur est {self.nom} \n la liste des top ten est {self.topten}"
+        return f"l'identifiant du joueur est {self.id}\nle nom du joueur est {self.nom}\nla liste des top ten est {self.topten}"
         
+j= Joueur(2,"oussama",[1,2,3])
+print(j)
