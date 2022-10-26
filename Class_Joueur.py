7a68922aef45f0fb0675da71e319f012b375ce6a
omarhfoul <oussama.marhfoul@eleve.ensai.fr>
2022-10-26 09:31:38 +0100

    modification classe joueur + création de la classe liste

--- Class_Joueur.py
+++
@@ -1,9 +0,0 @@
-class Joueur :
-    def __init__(self, id_joueur : int, nom_joueur : str, topten : list()) -> None:
-        self.id = id_joueur
-        self.nom = nom_joueur
-        self.topten = topten
-    
-    def __str__(self) -> str:
-        return f"l'identifiant du joueur est {self.id}\nle nom du joueur est {self.nom}\nla liste des top ten est {self.topten}"
-    
