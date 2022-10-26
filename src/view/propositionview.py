bfb2e1924bf1b32fb82ca1a5e86c153d0a1f947a
Mathis8 <mathis.david-quillot@eleve.ensai.fr>
2022-10-26 16:47:22 +0200

    Modification à la marge des vues de Propositions et connection

--- src/view/propositionview.py
+++ src/view/propositionview.py
@@ -4,11 +4,7 @@ from InquirerPy.base.control import Choice
 from src.view.abstractview import AbstractView
 from src.view.session import Session
 
-import signal
 ASK_PROPOSITION =inquirer.text(message = 'Quel mot veux tu proposer?')
-def handler(signum, frame):
-    print('Signal handler called with signal', signum)
-    raise OSError("Couldn't open device!")
 
 
 
@@ -19,9 +15,7 @@ class PropositionView(AbstractView) :
 
     def make_choice(self):
         proposition = ASK_PROPOSITION.execute()
-        # Set the signal handler and a 5-second alarm
-        signal.signal(signal.SIGALRM, handler)
-        signal.alarm(5)
+        
         print(proposition)
 
 
