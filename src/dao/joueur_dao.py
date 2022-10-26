d7cc3081fbc165b91f984eb2e92b13b32d19f07f
DINH Linh-da <id1966@DOMENSAI.ECOLE>
2022-10-26 08:40:51 +0200

    JoueurDao : ajout get_pseudo_by_id

--- src/dao/joueur_dao.py
+++ src/dao/joueur_dao.py
@@ -3,12 +3,69 @@ from src.dao.db_connection import DBConnection
 
 class JoueurDAO():
     ''
-    def creer(joueur):
+
+    def get_pseudo_by_id(self, id):
+
+        '''Méthode get_pseudo_by_id
+        
+        Permet de retourner le pseudo correspondant à un id_joueur
+        
+        Parameters
+        ----------
+        id : int
+            Identifiant du joueur
+        
+        Returns
+        --------
+        pseudo : 
+            Pseudo du joueur, None si le joueur n'est pas trouvé
+
+        '''
+
         connection = DBConnection().connection
         with connection.cursor() as cursor :
-            curseur.execute(
-                "INSERT INTO joueur(id_joueur, nom_joueur)"
-                " VALUES (id_joueur, nom_joueur)",
-                { "id_joueur" : joueur.id_joueur,
-                "nom_joueur" : joueur.nom_joueur}
+            cursor.execute(
+                "SELECT pseudo FROM joueur WHERE id_joueur = %(id)s"
+                , {"id": id}
             )
+
+            res = cursor.fetchone()
+
+        pseudo = None 
+
+        if res :
+            pseudo = res
+
+        return pseudo
+
+    def create(self, pseudo):
+
+        '''Méthode create
+        
+        Permet de créer un joueur
+        
+        Parameters
+        ----------
+        id : int
+            Identifiant du joueur
+        
+        pseudo : 
+            Pseudo saisi par le joueur
+
+        Returns
+        --------
+        joueur : 
+            
+
+        '''
+
+        connection = DBConnection().connection
+        with connection.cursor() as cursor :
+            cursor.execute(
+                "INSERT INTO joueur(pseudo)"
+                " VALUES (%(pseudo)s) RETURNING id_joueur"
+                ,{"pseudo": pseudo}
+            )
+            res = cursor.fetchone()
+
+        print(res)
