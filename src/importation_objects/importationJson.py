16d9f4fdf51663b15146aada2f9602f26aaa166f
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-10-26 11:54:06 +0200

    pause

--- src/importation_objects/importationJson.py
+++ src/importation_objects/importationJson.py
@@ -22,8 +22,11 @@ class ImportationJson(AbstractImportationListe):
         >>> import json
         >>> from src.importation_objects.abstract_importation_liste import AbstractImportationListe
         >>> ma_liste = ImportationJson()
+        >>> isinstance(ma_liste, ImportationJson)
+        True
         """
-        super.__init__(self)
+        super().__init__()
+
 
     def creer(self, fichier : str, dossier : str, encodage: str = ' utf-8'):
         """permet de créer une liste de mots à partir d'un fichier Json et après instanciation
@@ -46,8 +49,18 @@ class ImportationJson(AbstractImportationListe):
         --------
         >>> import json
         >>> from src.importation_objects.abstract_importation_liste import AbstractImportationListe
+        >>> ma_liste = ImportationJson()
+
         """
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+liste_exemple = {"mots":[
+    {"mot": "Apolinne"},
+    {"mot": "Linh-da"},
+    {"mot": "Mathieu"},
+    {"mot": "Mathis"},
+    {"mot": "Oussama"},
+]}
