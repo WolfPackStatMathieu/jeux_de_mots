72caaac60d38fdf50e52f13d704da8c07b2e67c3
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-10-26 10:51:39 +0200

    pause

--- src/importation_objects/importationJson.py
+++ src/importation_objects/importationJson.py
@@ -19,4 +19,30 @@ class ImportationJson(AbstractImportationListe):
         """
         super.__init__(self)
 
-    def creer(self, fichier : str, dossier, encodage)
+    def creer(self, fichier : str, dossier : str, encodage: str = ' utf-8'):
+        """permet de créer une liste de mots à partir d'un fichier Json et après instanciation
+        d'un objet ImportationJson.
+
+        Parameters
+        ----------
+        fichier : str
+            nom du fichier
+        dossier : str
+            chemin absolu du dossier où se trouve le fichier
+        encodage : str
+            encodage du fichier, par défaut UTF-8
+
+        Returns
+        -------
+        liste_mots : list[str]
+
+        Examples
+        --------
+        >>> import json
+        >>> from src.importation_objects.abstract_importation_liste import AbstractImportationListe
+        >>>
+        """
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
