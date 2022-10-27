b6766b27cb74ea2678410ee624c81323f58c49c3
GUERINEAU Apolline <id1980@DOMENSAI.ECOLE>
2022-10-27 16:35:10 +0200

    pycache

---
+++ api_kata/router/mot_get.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from fastapi import *
+from src.dao.mot_dao import MotDAO
+
+router = APIRouter(
+    prefix='/mot',
+    tags=['mot']
+)
+
+#Creer un mot
+@router.post("/contenu/{mot}")
+async def create_mot(mot):
+    mot_dao=MotDAO()
+    return(mot_dao.creer(mot))
+
+#Obetnir l'id d'un mot
+@router.get("/{mot}")
+async def create_mot(mot):
+    mot_dao=MotDAO()
+    return(mot_dao.get_id_by_mot(mot))
