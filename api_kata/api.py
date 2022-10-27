a895a982aa5993b1672ef13f8bc7a7d29bb107d7
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-10-27 10:08:38 +0200

    router /joueur

--- api_kata/api.py
+++ api_kata/api.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 import uvicorn
 from src.dao.joueur_dao import JoueurDAO
 kata=FastAPI()
 
-@kata.get("/")
+@kata.get("/",
+          tags=['accueil'],
+          summary='accueil de l api kata',
+          description='cette api permet de jouer avec des mots',
+          response_description='un message d accueil')
 async def root():
+
     return({"message":"bonjour"})
 
-@kata.get("/joueur/{id_joueur}")
+@kata.get("/joueur/{id_joueur}",
+          response_description='la string du pseudo du joueur demandé')
 async def get_joueur_by_id(id_joueur):
+    """permet de récupérer le pseudo d'un joueur par son id
+
+    - **id_joueur** paramètre de chemin obligatoire
+    """
     joueur_dao=JoueurDAO()
     return joueur_dao.get_pseudo_by_id(id_joueur)
 
@@ -16,7 +26,7 @@ async def get_joueur_by_id(id_joueur):
 async def get_joueur_by_id(pseudo):
     joueur_dao=JoueurDAO()
     return joueur_dao.create(pseudo)
-    
-    
+
+
 if __name__=='__main__':
     uvicorn.run(kata, host="127.0.0.1", port=80)
