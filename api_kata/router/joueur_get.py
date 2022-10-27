ed4572009b18a6559b20f36b3df5ec9e0b565a86
GUERINEAU Apolline <id1980@DOMENSAI.ECOLE>
2022-10-27 10:52:14 +0200

    router joueur

--- api_kata/router/joueur_get.py
+++ api_kata/router/joueur_get.py
@@ -7,6 +7,7 @@ router = APIRouter(
     tags=['joueur']
 )
 
+#Obtenir le pseudo d'un joueur
 @router.get("/{id_joueur}",
           response_description='la string du pseudo du joueur demandé')
 async def get_joueur_by_id(id_joueur):
@@ -16,3 +17,21 @@ async def get_joueur_by_id(id_joueur):
     """
     joueur_dao=JoueurDAO()
     return joueur_dao.get_pseudo_by_id(id_joueur)
+
+#Créer un joueur
+@router.post("/{pseudo}")
+async def create_joueur(pseudo):
+    joueur_dao=JoueurDAO()
+    joueur_dao.create(pseudo)
+
+#Obtenir le nom des listes d'un joueur
+@router.get("/{id_joueur}/liste")
+async def get_liste_by_id_joueur(id_joueur):
+    liste_dao=ListeDAO()
+    return liste_dao.get_liste_by_id_joueur(id_joueur)
+
+#Créer une liste associée à un joueur
+@router.create("/{id_joueur}/liste/{name}")
+async def create_by_name(name):
+    liste_dao=ListeDAO()
+    liste_dao.supprimer(id_joueur, name)
