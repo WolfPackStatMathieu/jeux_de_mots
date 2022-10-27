f6d7ba32059d1748949358b20a40fb4ca9b49a59
DINH Linh-da <id1966@DOMENSAI.ECOLE>
2022-10-27 17:46:15 +0200

    score_dao.py

--- src/dao/score_dao.py
+++ src/dao/score_dao.py
@@ -3,18 +3,29 @@ from src.dao.db_connection import DBConnection
 
 class ScoreDAO():
 
-    def creer(self, id_joueur, score):
+    def ajouter(self, id_joueur, score):
 
-        connection = DBConnection().connection
-        with connection.cursor() as cursor :
-            cursor.execute(
-                "INSERT INTO score(id_joueur, score)"
-                " VALUES (%(id_joueur)s, %(score)s ) ;", {"id_joueur": id_joueur, "score": score})
+        if score > ScoreDAO().get_dernier_meilleur_score(id_joueur):
 
-            cursor.execute("commit;")
+            id_score_a_supprimer = ScoreDAO().get_dernier_meilleur_score_id(id_joueur)
+
+            connection = DBConnection().connection
+            with connection.cursor() as cursor :
+                cursor.execute(
+                    "INSERT INTO score(id_joueur, score)"
+                    " VALUES (%(id_joueur)s, %(score)s ) ;", {"id_joueur": id_joueur, "score": score})
+                cursor.execute("commit;")
+
+            
+            #print(id_score_a_supprimer)
+            if 
+            ScoreDAO().supprimer(id_score_a_supprimer)
+        
+        else :
+            pass
 
  
-    def get_meilleur_score(self, id_joueur):
+    def get_dernier_meilleur_score(self, id_joueur):
 
         connection = DBConnection().connection
         with connection.cursor() as cursor :
@@ -24,10 +35,25 @@ class ScoreDAO():
                 "ORDER BY score ASC ;"
                 , {"id_joueur": id_joueur})
 
-            res = cursor.fetchone()
-            meilleur_score = res["score"]
+            res = cursor.fetchmany(10)
+            dernier_meilleur_score = res[0]['score']
 
-        return meilleur_score
+        return dernier_meilleur_score
+
+    def get_dernier_meilleur_score_id(self, id_joueur):
+
+        connection = DBConnection().connection
+        with connection.cursor() as cursor :
+            cursor.execute(
+                "SELECT id_score FROM score "
+                "WHERE id_joueur = (%(id_joueur)s) "
+                "ORDER BY score ASC ;"
+                , {"id_joueur": id_joueur})
+
+            res = cursor.fetchmany(10)
+            id_dernier_meilleur_score = res[0]['id_score']
+
+        return id_dernier_meilleur_score
 
 
     def supprimer(self, id_score):
@@ -41,7 +67,7 @@ class ScoreDAO():
 
             cursor.execute("commit;")
 
-    def get_top_10(self):
+    def get_top_10_general(self):
 
         connection = DBConnection().connection
         with connection.cursor() as cursor :
@@ -55,7 +81,23 @@ class ScoreDAO():
             for row in res:
                 liste.append(row["score"])
         return liste
+    
+    def get_top_10_perso(self, id_joueur):
 
+        connection = DBConnection().connection
+        with connection.cursor() as cursor :
+            cursor.execute(
+                "SELECT score"
+                " FROM score"
+                " WHERE id_joueur = (%(id_joueur)s)"
+                " ORDER BY score DESC ;"
+                ,{"id_joueur": id_joueur})
+                
+            res=cursor.fetchmany(10)
+            liste=[]
+            for row in res:
+                liste.append(row["score"])
+        return liste
             
 
     
