22789c8452349f48155a8a0d3c1e32f77c605e72
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-18 11:40:18 +0100

    pylint mot_get.py

--- api_kata/router/mot_get.py
+++ api_kata/router/mot_get.py
@@ -12,6 +12,19 @@ router = APIRouter(
 #Creer un mot
 @router.post("/contenu/{mot}")
 async def create_mot(mot: str =  Query(min_length=1, max_length=50,regex="^[A-Za-z]+$")):
+    """endpoint POST pour créer un mot en BDD
+
+    Parameters
+    ----------
+    mot : str
+        un mot à créer, by default Query(min_length=1, max_length=50,regex="^[A-Za-z]+$")
+        i.e. que des lettres majuscules ou minuscules
+
+    Returns
+    -------
+    function
+        lance la création du mot en BDD
+    """
     mot_dao=MotDAO()
     return mot_dao.creer(mot)
 
@@ -27,6 +40,18 @@ async def create_mot(mot: str =  Query(min_length=1, max_length=50,regex="^[A-Za
 #Obtenir l'id d'un mot
 @router.get("/mot/{mot}")
 async def get_id_by_mot(mot):
+    """endpoint GET pour obtenir un mot par son id
+
+    Parameters
+    ----------
+    mot : str
+        mot dont on veut l'id
+
+    Returns
+    -------
+    int
+        id du mot
+    """
     mot_dao=MotDAO()
     id_mot=mot_dao.get_id_by_mot(mot)
     return id_mot
