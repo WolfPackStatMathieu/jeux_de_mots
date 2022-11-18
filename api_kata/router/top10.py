455d25cb003171e8b96f609c4bb51c3a815c4634
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-18 11:49:15 +0100

    pylint top10.py

--- api_kata/router/top10.py
+++ api_kata/router/top10.py
@@ -1,5 +1,7 @@
+"""routeur contenant les endpoints à propos du top10 des scores
+"""
+
 from fastapi import APIRouter
-from fastapi import *
 from src.dao.score_dao import ScoreDAO
 
 router = APIRouter(
@@ -10,7 +12,13 @@ router = APIRouter(
 #Obtenir le top 10 général
 @router.get("")
 async def get_top10():
+    """endpont GET pour obtenir le top10 des scores
+
+    Returns
+    -------
+    List(List)
+        une liste de listes contenant chacune le score, l'id et le pseudo du joueur
+    """
     score_dao=ScoreDAO()
     top10 = score_dao.get_top_10_general()
-    return (top10)
-
+    return top10
